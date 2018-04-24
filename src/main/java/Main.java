528fcc834d0e370f7e7cf0690ce40425dbd6868f
sunaina078 <sunaina.krishnamoorthy@gmail.com>
2018-04-24 14:13:19 -0500

    Added the offset stuff, results are still not correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+// path for Sunaina = "/Users/sunaina/IdeaProjects/page-rank/src/main/resources/links"
+// path for Noah = "/Users/noah/Documents/Projects/IdeaProjects/SparkHello/src/main/resources/links"
+
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
@@ -12,12 +15,15 @@ public class Main {
 
     public static void main(String[] args) {
       // setup runtime
+
+      String path = "/Users/sunaina/IdeaProjects/page-rank/src/main/resources/links/";
+
       SparkConf conf = new SparkConf().setAppName("test app").setMaster("local");
       JavaSparkContext sc = new JavaSparkContext(conf);
       sc.setLogLevel("ERROR");
 
       // filename/links pairs
-      final JavaPairRDD<String, String> input = sc.wholeTextFiles("/Users/noah/Documents/Projects/IdeaProjects/SparkHello/src/main/resources/links");
+      final JavaPairRDD<String, String> input = sc.wholeTextFiles(path);
       final long numPages = input.count();
 
 
@@ -25,7 +31,7 @@ public class Main {
       // cleanup file names produce <URL, Contained URLS>
       final JavaPairRDD<String, String> pages = input.mapToPair(x -> {
         String url = x._1;
-        url = url.replace("file:/Users/noah/Documents/Projects/IdeaProjects/SparkHello/src/main/resources/links/", "");
+        url = url.replace("file:" + path, "");
         url = url.replace("%2f", "/");
         return new Tuple2<>(url, x._2);
         });
@@ -49,7 +55,7 @@ public class Main {
       final JavaRDD<String> allLinks = allPairs.map(x -> x._1).distinct();
 
       // 0 indexed
-      final long numLinks = allLinks.count() - 1;
+      final long numLinks = allLinks.count();
 
       final JavaPairRDD<String, Integer> zeroedWeightedLinks = allLinks.mapToPair(x -> new Tuple2<>(x, 0));
       final JavaPairRDD<String, Integer> weightedOutgoingLinks = urlPairs.mapToPair(x -> new Tuple2<>(x._1, 1));
@@ -73,57 +79,58 @@ public class Main {
       //URL to rank maping
       JavaPairRDD<String, Double> pageRanks = allLinks.mapToPair(x -> new Tuple2<>(x, new Double(1)));
 
-      for (int i = 0; i < 1; i++) {
+      for (int i = 0; i < 3; i++) {
         System.out.println("-------------- Iteration:" + i + " -------------");
         pageRanks.foreach(x -> System.out.println(x));
 
         //TODO need to do outerjoin in order to get sites that are sinks and deal with them
 
 
+        // Produces (url | cur page rank) of all pages without links
+        JavaPairRDD<String, Double> noOutgoingTemp = pageRanks
+                .join(outgoingCount)
+                .filter(x -> x._2._2 == 0)
+                .mapToPair(x -> new Tuple2<>(x._1, x._2._1));
+
+        Double total = noOutgoingTemp.map(x -> x._2).reduce((a, b) -> a + b);
+        Double offset = total / (numLinks - 1);
+
+        noOutgoingTemp = noOutgoingTemp.mapToPair(x -> new Tuple2<>(x._1, 0.0));
+
+        // Produces (url | cur page rank) of pages with outgoing links
+        JavaPairRDD<String, Tuple2<Double, Integer>> outgoingTempCount = pageRanks
+                .join(outgoingCount)
+                .filter(x -> x._2._2 >= 1);
+
+        JavaPairRDD<String, Double> outgoingTemp = outgoingTempCount
+                .mapToPair(x ->
+                        new Tuple2<>(x._1, x._2._1));
+
+
         //src link/(PR/#outgoing links)
-        JavaPairRDD<String, Double> weightedOutgoingPR = pageRanks
-            .join(outgoingCount)
+        JavaPairRDD<String, Double> weightedOutgoingPR = outgoingTempCount
             .mapToPair(x -> {
-          double denominator = x._2._2;
-          if (denominator == 0)
-            denominator = numLinks;
-          double effectivePR = x._2._1 / denominator;
+
+          System.out.println("Inside the map " + x);
+          double effectivePR = x._2._1 / x._2._2;
           return new Tuple2<>(x._1, effectivePR);
         });
-        System.out.println("---------------effective prs------------");
-
-        weightedOutgoingPR.foreach(x -> System.out.println(x));
+        //System.out.println("---------------weighted outgoing prs------------");
 
 
-        JavaRDD<Double> tempNoOutPR = weightedOutgoingPR
-            .join(noOutgoingLinks
-            .mapToPair(x -> new Tuple2<>(x, 0.0)))
-            .map(x -> x._2._1);
+        JavaPairRDD<String, Double> newPRs = outgoingTemp
+                .union(noOutgoingTemp)
+                .mapToPair(x ->
+                        new Tuple2<>(x._1, x._2 + offset));
 
-        //TODO add the offset to all pages, since all pr without outgoing links need to be redistributed
-        //TODO subtract prs from each page without links such that adding the offset will work
+        //weightedOutgoingPR.foreach(x -> System.out.println(x));
+
         //TODO also add dampening
 
-        tempNoOutPR.foreach(x -> System.out.println(x));
+        JavaPairRDD<String, Double> destPRs = weightedOutgoingPR.join(allPairs).mapToPair(x -> new Tuple2<>(x._2._2, x._2._1));
+        destPRs = destPRs.reduceByKey((a, b) -> a + b);
 
-        //JavaPairRDD<String, Tuple2<Double, Integer>> tempOutPR = effectiveSrcPR.join(weightedOutgoingLinks);
-
-        //Double offset = tempNoOutPR.reduce((a, b) -> a + b);
-
-
-        //dest link/incoming pr delta
-        JavaPairRDD<String, Double> destNewPR = weightedOutgoingPR.join(urlPairs).mapToPair(x -> new Tuple2<>(x._2._2, x._2._1));
-
-        System.out.println("---------------dest new pr------------");
-
-        destNewPR.foreach(x -> System.out.println(x));
-
-        //dest link/new pr
-        pageRanks = destNewPR.reduceByKey((a, b) -> a + b);
-        System.out.println("---------------page ranks------------");
-
-        pageRanks.foreach(x -> System.out.println(x));
-        System.out.println("---------------End of iteration------------");
+        pageRanks = destPRs;
       }
 
     }
