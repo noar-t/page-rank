3050aa1c2b8a6e38d6bdcc90cb2860b55086535b
sunaina078 <sunaina.krishnamoorthy@gmail.com>
2018-04-29 15:37:40 -0500

    input from user

--- src/main/java/Indexer.java
+++ src/main/java/Indexer.java
@@ -6,9 +6,7 @@ import org.apache.spark.api.java.function.*;
 import org.apache.spark.SparkConf;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 public class Indexer {
@@ -21,21 +19,27 @@ public class Indexer {
         sc.setLogLevel("ERROR");
 
 
+        Scanner key = new Scanner(System.in);
 
-        String[] keywordsStr = {"computer" , "education"};
+        System.out.print("Enter upto five words you want to search for: ");
+        String line = key.nextLine();
+        HashSet<String> keywordsStr= new HashSet<>(Arrays.asList(line.split(" ")));
+
+
+        //String[] keywordsStr = {"computer" , "education"};
 
 
         // filename/links pairs
-        final JavaPairRDD<String, String> input = sc.wholeTextFiles("file:///tmp/body");
+        final JavaPairRDD<String, String> input = sc.wholeTextFiles("/Users/sunaina/IdeaProjects/page-rank/src/main/resources/tmp/body");
+
 
         // cleanup file names
         final JavaPairRDD<String, String> pages = input.mapToPair(x -> {
             String url = x._1;
-            url = url.replace("file:/tmp/body/", "");
+            url = url.replace("file:/Users/sunaina/IdeaProjects/page-rank/src/main/resources/tmp/body/", "");
             url = url.replace("%2f", "/");
             return new Tuple2<>(url, x._2);
         });
-
 
         // url to keywords (removing , and .)
         JavaPairRDD<String, String> urlWords = pages.flatMapToPair(x -> {
@@ -51,7 +55,11 @@ public class Indexer {
             return pairs.listIterator();
         });
 
-        JavaPairRDD<String, String> matches = urlWords.filter(x -> Arrays.asList(keywordsStr).contains(x._1));
+
+
+        JavaPairRDD<String, String> matches = urlWords.filter(x -> keywordsStr.contains(x._1));
+
+        urlWords.foreach(x-> System.out.println(x));
 
         matches.foreach(x -> System.out.println(x._1));
         matches.foreach(x -> System.out.println(x._2));
